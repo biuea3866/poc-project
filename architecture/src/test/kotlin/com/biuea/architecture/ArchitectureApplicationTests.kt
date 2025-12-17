df0d6d092933c4000de5fa639432947e9963bb49
biuea <biuea3866@gmail.com>
2025-12-18 01:09:38 +0900

    [architecture:coupon] 카프카, 웹소켓, redis 플로우까지

---
+++ architecture/src/test/kotlin/com/biuea/architecture/ArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.biuea.architecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
